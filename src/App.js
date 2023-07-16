c113695ff9694d6d213e9e99a644bab781b3eb99
ShubhamBirari <shubham1851998@gmail.com>
2023-07-16 11:51:23 +0530

    banner added with hero image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css'
+import Hero from './components/Hero/Hero'
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Hero />
     </div>
   )
 }
